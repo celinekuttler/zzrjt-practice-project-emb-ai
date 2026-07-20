aee5fa027186f76dbddab48b3637c46c90382358
celinekuttler <194842828+celinekuttler@users.noreply.github.com>
2026-07-20 12:00:48 -0400

    linting done for sentiment analysis

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,15 +1,25 @@
-import requests
+""" sentiment analysis module """
 import json
-
+import requests
 
 def sentiment_analyzer(text_to_analyze):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    """ run a sentiment analysis on on text
+    Input (str): text to analyze 
+    Outputs: tuple (label, score) 
+        """
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/\
+    v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
     headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     myobj = {"raw_document":{"text":text_to_analyze}}
-    response = requests.post(url, json = myobj, headers=headers)
-    formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
-
+    response = requests.post(url, json = myobj, headers=headers, timeout=10)
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
+    else:
+        label = None
+        score = None
     return {'label':label,'score':score}
-
